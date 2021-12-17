1aed71420011aee66bc081331a1ed83ad50d9caa
Neil Armstrong <narmstrong@baylibre.com>
2021-12-17 15:44:03 +0000

    Add rollback feature to u-boot-ostree-scr-fit boot.cmd

--- recipes-bsp/u-boot/u-boot-ostree-scr-fit/meson-gx/boot.cmd
+++ recipes-bsp/u-boot/u-boot-ostree-scr-fit/meson-gx/boot.cmd
@@ -1,6 +1,20 @@
-load ${devtype} ${devnum}:2 ${scriptaddr} /boot/loader/uEnv.txt
-env import -t ${scriptaddr} ${filesize}
+# Default boot type and device
+setenv bootlimit 3
 
-load ${devtype} ${devnum}:2 ${kernel_addr_r} "/boot"${kernel_image}
+setenv bootcmd_resetvars 'setenv kernel_image; setenv bootargs; setenv kernel_image2; setenv bootargs2'
+setenv bootcmd_load_f 'ext4load ${devtype} ${devnum}:2 ${kernel_addr_r} "/boot"${kernel_image}'
+setenv bootcmd_run 'bootm ${kernel_addr_r}'
+setenv bootcmd_rollbackenv 'setenv kernel_image ${kernel_image2}; setenv bootargs ${bootargs2}'
+setenv bootcmd_set_rollback 'if test ! "${rollback}" = "1"; then setenv rollback 1; setenv upgrade_available 0; saveenv; fi'
+setenv bootostree 'run bootcmd_load_f; run bootcmd_run'
+setenv altbootcmd 'run bootcmd_set_rollback; if test -n "${kernel_image2}"; then run bootcmd_rollbackenv; fi; run bootostree; reset'
 
-bootm ${kernel_addr_r}
+if test ! -e ${devtype} ${devnum}:1 uboot.env; then saveenv; fi
+
+run bootcmd_resetvars
+ext4load ${devtype} ${devnum}:2 ${scriptaddr} /boot/loader/uEnv.txt
+env import -t ${scriptaddr} ${filesize} kernel_image bootargs kernel_image2 bootargs2
+
+if test "${rollback}" = "1"; then run altbootcmd; else run bootostree; if test ! "${upgrade_available}" = "1"; then setenv upgrade_available 1; saveenv; fi; reset; fi
+
+reset
